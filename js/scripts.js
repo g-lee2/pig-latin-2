3c6308319388cdfc876a2a4c825232ac5a91303b
Grace Lee <gracexsnoopy@gmail.com>
2022-11-01 15:02:47 -0700

    Fix functions to correctly translate to pig latin

--- js/scripts.js
+++ js/scripts.js
@@ -1,56 +1,68 @@
 function pigLatinVowels(input) {
   const inputArray = input.split(" ");
   const vowels = ["a", "e", "i", "o", "u"];
-  let resultArray = [];
   
   for (let i = 0; i < inputArray.length; i++) {
     for (let j = 0; j < vowels.length; j++) {
       if (inputArray[i][0] == vowels[j]) {
         let newWord = inputArray[i].toString() + "way";
-        resultArray.push(newWord);
+        return newWord.toUpperCase();
       }
     }
   }
-  return resultArray.join("").toUpperCase();
 };
 
-function pigLatinConsonants(input) {
+function pigLatinOneConsonant(input) {
   const inputArray = input.split(" ");
   const vowels = ["a", "e", "i", "o", "u"];
-  let resultArray = [];
   
   for (let i = 0; i < inputArray.length; i++) {
     for (let j = 0; j < vowels.length; j++) {
       if (inputArray[i][0] != vowels[j]) {
-        let newWord = inputArray[i].toString().slice(0) + inputArray[i][0] + "ay";
-        resultArray.push(newWord);
-      } else if (inputArray[i][1] != vowels[j]) {
-        let newWord = inputArray[i].toString().slice(1) + inputArray[i][1] + "ay";
-        resultArray.push(newWord);
-      } else if (inputArray[i][2] != vowels[j]) {
-        let newWord = inputArray[i].toString().slice(2) + inputArray[i][2] + "ay";
-        resultArray.push(newWord);
-      }
+        let newWord = inputArray[i].toString().slice(1) + inputArray[i][0] + "ay";
+        return newWord.toUpperCase();
+      } 
     }
   }
-  return resultArray.join("").toUpperCase();
+};
+
+function pigLatinTwoConsonants(input) {
+  const inputArray = input.split(" ");
+  const vowels = ["a", "e", "i", "o", "u"];
+  
+  for (let i = 0; i < inputArray.length; i++) {
+    for (let j = 0; j < vowels.length; j++) {
+      if (inputArray[i][0] != vowels[j] && inputArray[i][1] != vowels[j]) {
+        let newWord = inputArray[i].toString().slice(2) + inputArray[i][0] + inputArray[i][1] + "ay";
+        return newWord.toUpperCase();
+      } 
+    }
+  }
+};
+
+function pigLatinThreeConsonants(input) {
+  const inputArray = input.split(" ");
+  const vowels = ["a", "e", "i", "o", "u"];
+  
+  for (let i = 0; i < inputArray.length; i++) {
+    for (let j = 0; j < vowels.length; j++) {
+      if (inputArray[i][0] != vowels[j] && inputArray[i][1] != vowels[j] && inputArray[i][2] != vowels[j]) {
+        let newWord = inputArray[i].toString().slice(3) + inputArray[i][0] + inputArray[i][1] + inputArray[i][2] + "ay";
+        return newWord.toUpperCase();
+      } 
+    }
+  }
 };
 
 function pigLatinQu(input) {
   const inputArray = input.split(" ");
-  const except = ["q", "u"];
-  let resultArray = [];
-  
+
   for (let i = 0; i < inputArray.length; i++) {
-    if (inputArray[i][0] === except[0]) {
-      let newWord = inputArray[i].toString().slice(0) + inputArray[i][0];
-      resultArray.push(newWord);
-    } else if (inputArray[i][1] === except[1]) {
-      let newWord = inputArray[i].toString().slice(1) + inputArray[i][1] + "ay";
-      resultArray.push(newWord);
-    }
+    if (inputArray[i][0] == 'q' && inputArray[i][1] == 'u') {
+      let newWord = inputArray[i].toString().slice(2) + inputArray[i][0] + inputArray[i][1] + "ay";
+      return newWord.toUpperCase();
+    } 
   }
-  return resultArray.join("").toUpperCase();
 };
 
 window.addEventListener("load", function() {
@@ -61,8 +73,10 @@ window.addEventListener("load", function() {
 function handleFormSubmission(event) {
   event.preventDefault();
   const input = document.getElementById("inputSentence").value.toLowerCase();
-  pigLatinVowels(input);
-  pigLatinConsonants(input);
-  pigLatinQu(input);
   document.getElementById("result").removeAttribute("class");
-}
+  document.getElementById("translation-vowels").innerText = pigLatinVowels(input);
+  document.getElementById("translation-one-const").innerText = pigLatinOneConsonant(input);
+  document.getElementById("translation-two-const").innerText = pigLatinTwoConsonants(input);
+  document.getElementById("translation-three-const").innerText = pigLatinThreeConsonants(input);
+  document.getElementById("translation-qu").innerText = pigLatinQu(input);; 
+};
